89e07564ae47c363961ed0bf8cee29817abd70bb
MrZebarth <kris.zebarth@ocsb.ca>
2019-02-26 12:09:54 -0500

    Initial commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+#Change Calculator
+# Read in a cost
+# Read in the amount given
+# Calculate the change
+# Break the change into how many nickels, dimes, quarters
+# loonies, toonies, $5s, $10s, $20s, $50s, $100s
+# If amount is below the cost, say how much more they owe.
+cost=float(input("How much does the item cost?"))
